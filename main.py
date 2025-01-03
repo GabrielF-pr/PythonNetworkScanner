d4aff2534f9ad5f238b8be2d13d43a0ba5c85862
Gabriel <gabrielalcantara2705@gmail.com>
2025-01-03 13:31:36 +0000

    Port scanner improvements

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import socket
 
-def port_scan(host, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        for p in range(port, 0, -1):
+def port_scan():
+        host = input("Enter an IP address: ").strip()
+        start_port = int(input("Enter the starting port number: "))
+        end_port = int(input("Enter the ending port number: "))
+        
+        if start_port < 1 and  end_port > 65535 and start_port > end_port:
+            print("Invalid port range. Please enter a range between 1 and 65535.")
+
+        print(f"Scanning {host} from port {start_port} to {end_port}...")
+        for port in range(start_port, end_port+1):
             try:
-                sock.connect((host, p))
-                print(f"Port {p} opened!")
-            except:
-                pass
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                    sock.connect((host, port))
+                    print(f"{port} opened!")
+            except socket.error:
+                print(f"{port} closed")
+            except KeyboardInterrupt:
+                print("Scan interrupted!")
 
 def main():
-    host = input("Enter an IP address: ")
-    port = int(input("Enter a port number: "))
     
-
-    port_scan(host, port)
+    port_scan()
 
 
 if __name__ == "__main__":
