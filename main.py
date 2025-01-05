c7882189686811ffa80679eb4755f1462d0da535
Gabriel <gabrielalcantara2705@gmail.com>
2025-01-05 22:30:16 +0000

    Changed from sys.argv to argparse

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import socket
 import sys 
-
-CLOSED_PORTS = 0
+import argparse
 
 def checkargs():
-    global CLOSED_PORTS
-    for arg in range(1, len(sys.argv)):
-        if sys.argv[arg] == "-v":
-           CLOSED_PORTS = 1
+    parser = argparse.ArgumentParser()
 
-def port_scan():
+    parser.add_argument("-v", "--verbose", action="store_true")
+    return parser.parse_args()
+
+def port_scan(verbose=False):
         host = input("Enter an IP address: ").strip()
         start_port = int(input("Enter the starting port number: "))
         end_port = int(input("Enter the ending port number: "))
@@ -29,20 +28,19 @@ def port_scan():
                     print(f"{port} opened!")
                     open_ports += 1
             except socket.error:
-                if CLOSED_PORTS:
+                if verbose:
                     print(f"{port} closed")
                 closed_ports += 1
             except KeyboardInterrupt:
                 print("Scan interrupted!")
-                sock.close()
                 break
-        if CLOSED_PORTS:
+        if verbose:
             print(f"Total scanned ports: {open_ports + closed_ports}")
             print(f"Ports opened: {open_ports} | Ports closed: {closed_ports}")
 def main():
     
-    checkargs()
-    port_scan()
+    args = checkargs()
+    port_scan(verbose=args.verbose)
 
 
 if __name__ == "__main__":
