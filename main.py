6279fb0982f14acb8658463d9aaed2c20c98bf57
Gabriel <gabrielalcantara2705@gmail.com>
2025-01-05 00:36:21 +0000

    Added more debug informations

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def port_scan():
         if start_port < 1 or  end_port > 65535 or start_port > end_port:
             print("Invalid port range. Please enter a range between 1 and 65535.")
             return
+        open_ports = 0
+        closed_ports = 0
 
         print(f"Scanning {host} from port {start_port} to {end_port}...")
         for port in range(start_port, end_port+1):
@@ -25,14 +27,18 @@ def port_scan():
                     sock.settimeout(1)
                     sock.connect((host, port))
                     print(f"{port} opened!")
+                    open_ports += 1
             except socket.error:
                 if CLOSED_PORTS:
                     print(f"{port} closed")
+                closed_ports += 1
             except KeyboardInterrupt:
                 print("Scan interrupted!")
                 sock.close()
                 break
-
+        if CLOSED_PORTS:
+            print(f"Total scanned ports: {open_ports + closed_ports}")
+            print(f"Ports opened: {open_ports} | Ports closed: {closed_ports}")
 def main():
     
     checkargs()
